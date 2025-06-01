f774299c929176d7095dca8d2e8d9e3e7f242c8f
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2025-06-01 16:11:05 +0530

    Time: 5 ms (69.99%), Space: 44 MB (62.4%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x >= 0) {
+            int n = x;
+            int rev = 0;
+            while (n != 0) {
+                rev = rev * 10 + n % 10;
+                n /= 10;
+            }
+
+            if (rev == x) {
+                return true;
+            } else {
+                return false;
+            }
+        }else{
+            return false;
+        }
+
+    }
+}
